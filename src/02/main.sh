820a09bf4796b34879c72bd429e038a1e2a9555a
Stanley Kudrow <stankudrow@noreply.com>
2023-03-30 12:56:08 +0300

    02: printf's removed

--- src/02/main.sh
+++ src/02/main.sh
@@ -9,18 +9,15 @@ function main()
     read -r -p "Saving data? " to_save
     if [[ $to_save =~ ^[Yy]$ ]];
     then
-        printf "YES\n"
         read -r -p "Into a file? " save_to_file
         if [[ $save_to_file =~ ^[Yy]$ ]];
         then
-            printf "FILE YES\n"
             file_name=$(date +"%d_%m_%y_%H_%M_%S")
             print_stats > "${file_name}.status"
         else
             read -r -p "Into a standard input? " save_to_stdin
             if [[ $save_to_stdin =~ ^[Yy]$ ]];
             then
-                printf "STDIN YES\n"
                 print_stats
             fi
         fi
