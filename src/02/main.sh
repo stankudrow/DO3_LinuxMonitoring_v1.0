59fe7572514c5af52cfc0c249fabbb91f4eed7a5
Stanley Kudrow <stankudrow@noreply.com>
2023-04-13 12:28:26 +0300

    prompt with [yY]

--- src/02/main.sh
+++ src/02/main.sh
@@ -6,16 +6,16 @@ source ./statician.sh
 
 function main()
 {
-    read -r -p "Saving data? " to_save
+    read -r -p "Saving data? [yY] " to_save
     if [[ $to_save =~ ^[Yy]$ ]];
     then
-        read -r -p "Into a file? " save_to_file
+        read -r -p "Into a file? [yY] " save_to_file
         if [[ $save_to_file =~ ^[Yy]$ ]];
         then
             file_name=$(date +"%d_%m_%y_%H_%M_%S")
             print_stats > "${file_name}.status"
         else
-            read -r -p "Into a standard input? " save_to_stdin
+            read -r -p "Into a standard input? [yY] " save_to_stdin
             if [[ $save_to_stdin =~ ^[Yy]$ ]];
             then
                 print_stats
